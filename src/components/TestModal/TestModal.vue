45cc24ff2e3c13ecc5b6b2eb139220e97dc5e533
Eduardo Fernandes <eduardo.scosta@me.com.br>
2025-08-11 15:04:04 -0300

    Test Modal

--- src/components/TestModal/TestModal.vue
+++ src/components/TestModal/TestModal.vue
@@ -1,24 +1,27 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import {ref} from 'vue'
+
 const first = ref(false)
 const second = ref(false)
 </script>
 
 <template>
-  <UModal v-model:open="first" title="First modal" :ui="{ footer: 'justify-end' }">
-    <UButton color="neutral" variant="subtle" label="Open" />
+  <teleport to="body">
+    <UModal v-model:open="first" title="First modal" :ui="{ footer: 'justify-end' }">
+      <UButton color="neutral" variant="subtle" label="Open"/>
 
-    <template #footer>
-      <UButton label="Close" color="neutral" variant="outline" @click="first = false" />
+      <template #footer>
+        <UButton label="Close" color="neutral" variant="outline" @click="first = false"/>
 
-      <UModal v-model:open="second" title="Second modal" :ui="{ footer: 'justify-end' }">
-        <UButton label="Open second" color="neutral" />
+        <UModal v-model:open="second" title="Second modal" :ui="{ footer: 'justify-end' }">
+          <UButton label="Open second" color="neutral"/>
 
-        <template #footer>
-          <UButton label="Close" color="neutral" variant="outline" @click="second = false" />
-        </template>
-      </UModal>
-    </template>
-  </UModal>
+          <template #footer>
+            <UButton label="Close" color="neutral" variant="outline" @click="second = false"/>
+          </template>
+        </UModal>
+      </template>
+    </UModal>
+  </teleport>
 </template>
 
