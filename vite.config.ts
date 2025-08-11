2bf3808705a33c948a40e055914d04b753acb383
Eduardo Fernandes <eduardo.scosta@me.com.br>
2025-08-11 18:22:50 -0300

    MW-00000 - feat: configure dual repositories and clean up files

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import ui from '@nuxt/ui/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue({
