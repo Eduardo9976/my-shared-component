ae5c872d003715b6ff5bbca11aab7c5b9b765b80
Eduardo Fernandes Silvino Costa <eduardo.scosta@me.com.br>
2025-08-27 12:20:17 -0300

    feat(ui): Add webcomponent reset styles
    Adds a reset CSS file to provide a consistent base style for
    web components, preventing style inheritance issues.  Imports and
    applies these styles in the TheHeader component.

--- src/components/TheHeader/the-header.ts
+++ src/components/TheHeader/the-header.ts
@@ -1,11 +1,12 @@
 import {defineCustomElement} from 'vue'
 import TheHeader from './TheHeader.ce.vue'
+import resetStyles from '../../assets/webcomponent-reset.css?inline'
 import tailwindStyles from '../../assets/main.css?inline'
 import meIconStyles from '@mercadoeletronico/me-icon/dist/css/me-icon.min.css?inline'
 
 const TheHeaderElement = defineCustomElement(TheHeader, {
   shadowRoot: true,
-  styles: [meIconStyles, tailwindStyles]
+  styles: [resetStyles, meIconStyles, tailwindStyles]
 })
 
 if (!customElements.get('the-header')) {
