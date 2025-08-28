b1f75f878e3c539dbc5076e6655e177ef491c25e
Eduardo Fernandes Silvino Costa <eduardo.scosta@me.com.br>
2025-08-28 11:31:47 -0300

    testando build de estilos

--- src/components/TheHeader/the-header.ts
+++ src/components/TheHeader/the-header.ts
@@ -1,11 +1,17 @@
 import {defineCustomElement} from 'vue'
 import TheHeader from './TheHeader.ce.vue'
+import webcomponentStyles from '../../../dist/webcomponents-styles.css?inline'
 import tailwindStyles from '../../assets/main.css?inline'
 import meIconStyles from '@mercadoeletronico/me-icon/dist/css/me-icon.min.css?inline'
 
+const fixedCSS = webcomponentStyles.replace(
+  /^(\s*):root\s*\{(?![^}]*:host)/gm,
+  '$1:root, :host {'
+)
+
 const TheHeaderElement = defineCustomElement(TheHeader, {
   shadowRoot: true,
-  styles: [meIconStyles, tailwindStyles]
+  styles: [fixedCSS, meIconStyles, tailwindStyles]
 })
 
 if (!customElements.get('the-header')) {
