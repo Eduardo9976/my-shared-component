8be9a6f88ee979b5b165848c5f21b09d2880eded
Eduardo Fernandes Silvino Costa <eduardo.scosta@me.com.br>
2025-08-28 13:15:46 -0300

    testando build de estilos

--- src/components/TheHeader/the-header.ts
+++ src/components/TheHeader/the-header.ts
@@ -1,11 +1,12 @@
 import {defineCustomElement} from 'vue'
 import TheHeader from './TheHeader.ce.vue'
 import tailwindStyles from '../../assets/main.css?inline'
+import antiReboot from '../../assets/anti-bootstrap-reboot.css?inline'
 import meIconStyles from '@mercadoeletronico/me-icon/dist/css/me-icon.min.css?inline'
 
 const TheHeaderElement = defineCustomElement(TheHeader, {
   shadowRoot: true,
-  styles: [meIconStyles, tailwindStyles]
+  styles: [antiReboot, meIconStyles, tailwindStyles]
 })
 
 if (!customElements.get('the-header')) {
