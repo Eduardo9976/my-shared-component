5ba005642d21a3de8b71deb4d0406e76534737e7
Eduardo Fernandes Silvino Costa <eduardo.scosta@me.com.br>
2025-08-28 14:02:46 -0300

    chore: Remove anti-bootstrap-reboot.css
    Removes the `anti-bootstrap-reboot.css` file and its import
    from `the-header.ts` to avoid duplicate styles since tailwind
    css already implements the "reboot" styles.

--- src/components/TheHeader/the-header.ts
+++ src/components/TheHeader/the-header.ts
@@ -1,12 +1,11 @@
 import {defineCustomElement} from 'vue'
 import TheHeader from './TheHeader.ce.vue'
 import tailwindStyles from '../../assets/main.css?inline'
-import antiReboot from '../../assets/anti-bootstrap-reboot.css?inline'
 import meIconStyles from '@mercadoeletronico/me-icon/dist/css/me-icon.min.css?inline'
 
 const TheHeaderElement = defineCustomElement(TheHeader, {
   shadowRoot: true,
-  styles: [antiReboot, meIconStyles, tailwindStyles]
+  styles: [meIconStyles, tailwindStyles]
 })
 
 if (!customElements.get('the-header')) {
