61dcaa6a7e4ed4481d8fd836148fdc5dd3bc14db
Eduardo Fernandes Silvino Costa <eduardo.scosta@me.com.br>
2025-09-19 09:42:32 -0300

    test build

--- dist/the-header.js
+++ dist/the-header.js
@@ -30150,19 +30150,6 @@ const tailwindStyles = `/*! tailwindcss v4.1.11 | MIT License | https://tailwind
   }
 }
 
-:host {
-  all: initial;
-  font-family: inherit;
-  font-size: inherit;
-  line-height: inherit;
-  color: inherit;
-  display: block;
-}
-
-:host * {
-  all: revert-layer;
-}
-
 @layer theme {
   :root, :host {
     --font-sans: "Roboto", sans-serif;
